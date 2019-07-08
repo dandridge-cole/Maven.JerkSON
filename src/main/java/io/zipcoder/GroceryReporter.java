f7846fbbdbc51b743041b081f049e4d6c8b0f787
Dandridge Cole <dandridgecole@yahoo.com>
2019-07-08 16:50:10 -0400

    All tests passing except for toStringTest

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,16 +1,50 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.CASE_INSENSITIVE;
 
 public class GroceryReporter {
     private final String originalFileText;
+    private ItemParser ip = new ItemParser();
+    List<Item> items = new ArrayList<>();
+    Map<String,List<Double>> map = new LinkedHashMap<>();
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
     }
 
+    public void processInput(){
+        this.items = ip.parseItemList(this.originalFileText);
+    }
+
+    public void buildMap(){
+        for(int i=0; i<items.size();i++){
+      //      Pattern nameP = Pattern.compile(item.getName(), CASE_INSENSITIVE);
+            String thisName = items.get(i).getName();
+            if(this.map.containsKey(thisName)){
+                List<Double> temp = map.get(thisName);
+                temp.add(items.get(i).getPrice());
+            }
+            else map.put(thisName,Arrays.asList(items.get(i).getPrice()));
+        }
+    }
+
+
     @Override
     public String toString() {
-        return null;
+        processInput();
+        buildMap();
+        StringBuilder sb = new StringBuilder();
+        List<String> keys = new ArrayList<>(this.map.keySet());
+        for(int i=0; i<this.map.size();i++){
+            sb.append("name:").append(String.format("%8s \t\t seen: %d times",keys.get(i),this.map.get(keys.get(i)).size()))
+            .append("\n============= \t \t =============");
+        }
+        return sb.toString();
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,67 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.CASE_INSENSITIVE;
 
 public class ItemParser {
+    private Integer exceptions=0;
+
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        List<String> itemStrings = divideList(valueToParse);
+        List<Item> itemList = new ArrayList<>();
+
+        for(String s: itemStrings){
+            try{
+                Item item = parseSingleItem(s);
+                itemList.add(item);
+            } catch (ItemParseException ipe){
+                this.exceptions++;
+                System.out.println("Exception # "+this.exceptions);
+            }
+        }
+        return itemList;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        Pattern p = Pattern.compile("[;(##)]");
+        List<String> fields = Arrays.asList(p.split(singleItem));
+        if (fields.size()!=4) throw new ItemParseException();
+        String name="";
+        Double price=0.0;
+        String type="";
+        String expiration="";
+        p=Pattern.compile("[:@^*%]");
+        Pattern nameP = Pattern.compile("name", CASE_INSENSITIVE);
+        Pattern typeP = Pattern.compile("type", CASE_INSENSITIVE);
+        Pattern expP = Pattern.compile("expiration", CASE_INSENSITIVE);
+        Pattern priceP = Pattern.compile("price", CASE_INSENSITIVE);
+        for(String s:fields){
+            String[] field = p.split(s);
+            if(field.length!=2) throw new ItemParseException();
+            if(nameP.matcher(field[0]).matches()) name=field[1].toLowerCase();
+            else if (typeP.matcher(field[0]).matches()) type=field[1].toLowerCase();
+            else if (expP.matcher(field[0]).matches()) expiration=field[1];
+            else if (priceP.matcher(field[0]).matches()) {
+                try {
+                    price = Double.parseDouble(field[1]);
+                } catch (NumberFormatException nfe){
+                    throw new ItemParseException();
+                }
+            }
+            else throw new ItemParseException();
+        }
+        return new Item(name,price,type,expiration);
+    }
+
+    private List<String> divideList(String list) {
+        Pattern p = Pattern.compile("##");
+
+        return Arrays.asList(p.split(list));
     }
 }
